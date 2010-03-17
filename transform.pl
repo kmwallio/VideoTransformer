af9b68cada966b065e591f3920b19e4742b8ba68
Miles Wallio <mwallio@gmail.com>
2010-03-17 17:09:04 -0400

    Setting up layouts

--- transform.pl
+++ transform.pl
@@ -7,6 +7,9 @@ my $window = Gtk2::Window->new('toplevel');
 $window->set_title("Video Transfomer - Control Window");
 $window->signal_connect('delete-event' => sub{ Gtk2->main_quit });
 
+# General layout thing
+my $pane = Gtk2::VBox->new();
+
 # Add a quit button
 my $quit_b = Gtk2::Button->new("Quit");
 $quit_b->signal_connect('clicked' => sub{ Gtk2->main_quit });
@@ -16,7 +19,9 @@ my $menu_b = Gtk2::HBox->new();
 
 $menu_b->pack_start($quit_b, TRUE, TRUE, 0); 
 
-$window->add($menu_b);
+$pane->pack_end($menu_b, TRUE, FALSE, 0);
+
+$window->add($pane);
 $window->show_all;
 
 Gtk2->main;
