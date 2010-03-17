19ed3ee636d8f32354085f31d9b73ea6db3a67d9
Miles Wallio <mwallio@gmail.com>
2010-03-17 16:56:19 -0400

    Leaning Gtk2-Perl

--- transform.pl
+++ transform.pl
@@ -4,10 +4,12 @@ use Gtk2 '-init';
 
 # Create our Window
 $window = Gtk2::Window->new('toplevel');
-$window->set_title("Video Transfomer");
+$window->set_title("Video Transfomer - Control Window");
+$window->signal_connect('delete-event' => sub{ Gtk2->main_quit });
 
 # Add a quit button
 $button = Gtk2::Button->new("Quit");
+$button->signal_connect('clicked' => sub{ Gtk2->main_quit });
 
 $window->show_all;
 
