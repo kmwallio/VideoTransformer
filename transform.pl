27f399c15d9d97e0c242ea796a076b1561759bd8
Miles Wallio <mwallio@gmail.com>
2010-03-17 17:23:59 -0400

    Adding components

--- transform.pl
+++ transform.pl
@@ -9,21 +9,22 @@ $window->signal_connect('delete-event' => sub{ Gtk2->main_quit });
 
 # General layout thing
 my $pane = Gtk2::VBox->new();
-
-# Add a quit button
-my $quit_b = Gtk2::Button->new("Quit");
-$quit_b->signal_connect('clicked' => sub{ Gtk2->main_quit });
-
-# Bottom menu buttons
-my $menu_b = Gtk2::HBox->new();
-
-$menu_b->pack_start($quit_b, TRUE, TRUE, 0); 
+	
+	# File choosers...
+	
+	# Bottom menu buttons
+	my $menu_b = Gtk2::HBox->new();
+	# Add a quit button
+	my $quit_b = Gtk2::Button->new("Quit");
+	$quit_b->signal_connect('clicked' => sub{ Gtk2->main_quit });
+	# Add our buttons
+	$menu_b->pack_start($quit_b, TRUE, TRUE, 0); 
 
 $pane->pack_end($menu_b, TRUE, FALSE, 0);
 
+# Compose and show the window.
 $window->add($pane);
 $window->show_all;
 
 Gtk2->main;
-
 0;
