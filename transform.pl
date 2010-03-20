241c345abf8810697f94211ed9b0e35379cd5939
Miles Wallio <mwallio@gmail.com>
2010-03-20 17:06:02 -0400

    Actually getting somewhere :)

--- transform.pl
+++ transform.pl
@@ -5,9 +5,11 @@ use Gtk2 '-init';
 use Glib qw/TRUE FALSE/;
 
 # Create our Window
-my $window = Gtk2::Window->new('toplevel');
+our $window = Gtk2::Window->new('toplevel');
 $window->set_title("Video Transfomer - Control Window");
 $window->signal_connect('delete-event' => sub{ Gtk2->main_quit });
+
+our $working_vid = FALSE;
 
 # General layout thing
 my $pane = Gtk2::VBox->new();
@@ -43,8 +45,17 @@ my $pane = Gtk2::VBox->new();
 	# Add a quit button
 	my $quit_b = Gtk2::Button->new("Quit");
 	$quit_b->signal_connect('clicked' => sub{ Gtk2->main_quit });
+	# Process
+	my $mouse_b = Gtk2::Button->new("Click-Through Process");
+	$mouse_b->signal_connect('clicked' => sub{ click_through() });
+	
+	my $magic_b = Gtk2::Button->new("Magic Process");
+	$magic_b->signal_connect('clicked' => sub{ magic_through() });
+	
 	# Add our buttons
-	$menu_b->pack_start($quit_b, TRUE, TRUE, 0); 
+	$menu_b->pack_start($mouse_b, TRUE, TRUE, 0);
+	$menu_b->pack_start($magic_b, TRUE, TRUE, 0);
+	$menu_b->pack_end($quit_b, TRUE, TRUE, 0); 
 
 $pane->pack_start($chooser, TRUE, TRUE, 0);
 $pane->pack_end($menu_b, TRUE, FALSE, 0);
@@ -58,13 +69,12 @@ Gtk2->main();
 sub choose_file {
 	my ($prompt, $type, $where) = @_;
 	# Create a new file chooser dialog
-	my $file_chooser = Gtk2::FileChooserDialog->new(
-								$prompt,
-								undef,
-								$type,
-								'gtk-cancel' => 'cancel',
-								'gtk-ok' => 'ok'
-							);
+	my $file_chooser = Gtk2::FileChooserDialog->new($prompt,
+												undef,
+												$type,
+												'gtk-cancel' => 'cancel',
+												'gtk-ok' => 'ok'
+											);
 	# Only let movies be selected
 	$file_chooser->add_filter(&filter_movie());
 	
@@ -78,15 +88,15 @@ sub choose_file {
 	
 	if ($where eq 'source'){
 		$source_file = $fname;
-		if ($source_file =~ m/\/(.*?)\./){
-			$lbl_source->set_text($1);
+		if ($source_file =~ m/\/(.*)\/(.*)\.(.*)/){
+			$lbl_source->set_text($2 . "." . $3);
 		}else{
 			$lbl_source->set_text("No File Selected");
 		}
 	}else{
 		$dest_file = $fname;
-		if ($dest_file =~ m/\/(.*?)\./){
-			$lbl_dest->set_text($1);
+		if ($dest_file =~ m/\/(.*)\/(.*)\.(.*)/){
+			$lbl_dest->set_text($2 . "." . $3);
 		}else{
 			$lbl_dest->set_text("No File Selected");
 		}
@@ -99,6 +109,31 @@ sub filter_movie{
 	$filter->set_name("Videos");
 	$filter->add_mime_type("video/quicktime");
 	$filter->add_mime_type("video/mpeg");
-	$filter->add_mime_type("x-msvideo");
+	$filter->add_mime_type("video/x-msvideo");
 	return $filter;
+}
+
+sub click_through {
+	if ($working_vid){
+		return already_working();
+	}
+	$working_vid = TRUE;
+}
+
+sub magic_through {
+	if ($working_vid){
+		return already_working();
+	}
+	$working_vid = TRUE;
+}
+
+sub already_working {
+	my $dialog = Gtk2::MessageDialog->new ($window,
+										'modal',
+										'error',
+										'ok',
+										'Already processing the videos.');
+	$dialog->run();
+	$dialog->destroy();
+	return FALSE;
 }
