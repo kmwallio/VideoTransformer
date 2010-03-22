67c1a06cf4a2a8c3547385d23cda8d4c43e09ca4
Miles Wallio <mwallio@gmail.com>
2010-03-22 18:25:16 -0400

    Almost done.  Just need to recompile into video and interpolate

--- transform.pl
+++ transform.pl
@@ -5,13 +5,23 @@ use Gtk2 '-init';
 use Glib qw/TRUE FALSE/;
 use KMVid::Clicker;
 use KMVid::ExtractFrames;
-use GD;
+use Image::Magick;
+use PDL;
+use PDL::NiceSlice;
+use PDL::Matrix;
+use PDL::MatrixOps;
 
 # Create our Window
 our $window = Gtk2::Window->new('toplevel');
 $window->set_title("Video Transfomer - Control Window");
 $window->signal_connect('delete-event' => sub{ Gtk2->main_quit });
 $window->set_border_width(15);
+
+$magic_window->set_title('Processing Frames:');
+my $mpBar = Gtk2::ProgressBar->new();
+$magic_window->signal_connect('delete-event' => sub{ $magic_window->hide_all() });
+$magic_window->add($mpBar);
+$magic_window->resize(500, 100);
 
 our $working_vid = FALSE;
 
@@ -31,6 +41,7 @@ my $pane = Gtk2::VBox->new();
 	our $dest_file = "";
 	our @frames;
 	our $cur_frame = 0;
+	our @clicks;
 	
 	my $open_dest = Gtk2::Button->new('Select Destination Video');
 	$open_dest->signal_connect('clicked' => sub{ choose_file('Select Destination Video','open','dest') });
@@ -60,7 +71,7 @@ my $pane = Gtk2::VBox->new();
 	
 	# Add our buttons
 	$menu_b->pack_start($mouse_b, TRUE, TRUE, 0);
-	$menu_b->pack_start($magic_b, TRUE, TRUE, 0);
+	#$menu_b->pack_start($magic_b, TRUE, TRUE, 0);
 	$menu_b->pack_end($quit_b, TRUE, TRUE, 0); 
 
 $pane->pack_start($chooser, TRUE, TRUE, 0);
@@ -130,8 +141,10 @@ sub click_through {
 	$working_vid = TRUE;
 	my $dest = new ExtractFrames($dest_file, "dest");
 	$dest->start();
+	$dest = undef;
 	my $source = new ExtractFrames($source_file, "source");
 	$source->start();
+	$source = undef;
 	
 	opendir(DIR, "./temp/dest");
 	@frames = readdir(DIR);
@@ -159,17 +172,24 @@ sub click_through {
 	#		$cl = undef;
 	#	}
 	#}
-	
-	$working_vid = FALSE;
 	return;
 }
 
 sub next_frame {
 	$cur_frame = $cur_frame + 1;
-	print $frames[$cur_frame] ."\n";
-	my $cl = new Clicker("./temp/dest/" . $frames[$cur_frame], \&next_frame);
-	$cl->start();
-	$cl = undef;
+	my $size = scalar(@frames);
+	if($cur_frame < $size){
+		my $s_frame = './temp/source/' . $frames[$cur_frame];
+		if(-e $s_frame){
+			my $cl = new Clicker("./temp/dest/" . $frames[$cur_frame], \&next_frame);
+			$cl->start();
+			$cl = undef;
+		}else{
+			work_magic();
+		}
+	}else{
+		work_magic();
+	}
 }
 
 sub magic_through {
@@ -209,16 +229,126 @@ sub no_input {
 	return FALSE;
 }
 
+sub work_magic {
+	$magic_window->show_all();
+	open(CLICKR, "< ./temp/clicks.log");
+	while(<CLICKR>){
+		push(@clicks, $_);
+	}
+	close(CLICKR);
+	foreach(@frames){
+		if($_ =~ m/(jpeg|jpg)/){
+			$mpBar->set_fraction($current / scalar(@frames));
+			$mpBar->set_text(($current - 2) . ' of ' . (scalar(@frames) - 2));
+			while (Gtk2->events_pending) {
+				Gtk2->main_iteration;
+			}
+			Gtk2::Gdk->flush;
+			my $s_frame = './temp/source/' . $_;
+			if(-e $s_frame){
+				transform('./temp/dest/' . $_, $s_frame, './temp/output/' . $_, shift(@clicks));
+			}
+		}
+	}
+}
+
 sub transform {
 	my $dest = shift;
 	my $source = shift;
 	my $out = shift;
 	my $coords = shift;
 	
-	my $dest_img = GD::Image->newFromJpeg($dest);
-	my $source_img = GD::Image->newFromJpeg($source);
+	($x3, $y3, $x2, $y2, $x0, $y0, $x1, $y1) = split(/\s/, $coords);
 	
-	open(OUTFILE, ">" . $out);
-	print OUTFILE $image->jpeg(100);
-	close(OUTFILE);
+	my $dest_img = new Image::Magick;
+	$dest_img->Read($dest);
+	my $source_img = new Image::Magick;
+	$source_img->Read($source);
+	
+	my $width = $source_img->Get('columns');
+	my $height = $source_img->Get('rows');
+	
+	my $nwidth = $dest_img->Get('columns');
+	my $nheight = $dest_img->Get('rows');
+	
+	my $u0 = 0;
+	my $v0 = $height;
+	my $u1 = $width;
+	my $v1 = $height;
+	my $u2 = $width;
+	my $v2 = 0;
+	my $u3 = 0;
+	my $v3 = 0;
+	
+	my $m = PDL::Matrix->pdl([
+		[$u0, $v0, 1, 0, 0, 0, (-1 * $u0 * $x0), (-1 * $v0 * $x0)],
+		[$u1, $v1, 1, 0, 0, 0, (-1 * $u1 * $x1), (-1 * $v1 * $x1)],
+		[$u2, $v2, 1, 0, 0, 0, (-1 * $u2 * $x2), (-1 * $v2 * $x2)],
+		[$u3, $v3, 1, 0, 0, 0, (-1 * $u3 * $x3), (-1 * $v3 * $x3)],
+		[0, 0, 0, $u0, $v0, 1, (-1 * $u0 * $y0), (-1 * $v0 * $y0)],
+		[0, 0, 0, $u1, $v1, 1, (-1 * $u1 * $y1), (-1 * $v1 * $y1)],
+		[0, 0, 0, $u2, $v2, 1, (-1 * $u2 * $y2), (-1 * $v2 * $y2)],
+		[0, 0, 0, $u3, $v3, 1, (-1 * $u3 * $y3), (-1 * $v3 * $y3)]
+		]);
+	
+	my $s0 = PDL::Matrix->pdl([
+		[$x0],
+		[$x1],
+		[$x2],
+		[$x3],
+		[$y0],
+		[$y1],
+		[$y2],
+		[$y3]
+		]);
+	my $s = PDL::Matrix->pdl($x0,
+		$x1,
+		$x2,
+		$x3,
+		$y0,
+		$y1,
+		$y2,
+		$y3);
+	
+	my $m_inv = inv($m);
+	my $q = $m x $s0;
+	#print $m;
+	#print $m_inv;
+	my $r = (transpose($m_inv) x $s);
+	$r = transpose($r);
+	my ($a, $b, $c, $d, $e, $f, $g, $h) = list($r);
+	
+	my $mm = PDL::Matrix->pdl([
+		[$e - ($f * $h), ($c * $h) - $b, ($b * $f) - ($c * $e)],
+		[($f * $g) - $d, $a - ($c * $g), ($c * $d) - ($a * $f)],
+		[($d * $h) - ($e * $g), ($b * $g) - ($a * $h), ($a * $e) - ($b * $d)]
+		]);
+	my @nColor = (-1, -1, -1);
+	my @oColor = (-1, -1, -1);
+	for($nX = 0; $nX < $nwidth; $nX++){
+		for($nY = 0; $nY < $nheight; $nY++){
+			my $ohYeah = PDL::Matrix->pdl([[$nX], [$nY], [1]]);
+			my ($oX, $oY, $oW) = list($mm x $ohYeah);
+			$oX = $oX / $oW;
+			$oY = $oY / $oW;
+			if(int($oX) >= 0 && int($oX) < $width && int($oY) >= 0 && int($oY) < $height){
+				@nColor = $source_img->GetPixel(channel => 'RGB', x => int($oX), y => int($oY));
+				@oColor = $dest_img->GetPixel(x => $nX, y => $nY);
+				$dest_img->SetPixel(x => $nX, y => $nY, color => \@nColor);
+			}
+			$ohYeah = undef;
+			while (Gtk2->events_pending) {
+				Gtk2->main_iteration;
+			}
+			Gtk2::Gdk->flush;
+		}
+	}
+	$s = undef;
+	$s0 = undef;
+	$m_inv = undef;
+	$q = undef;
+	$m = undef;
+	$dest_img->Write($out);
+	$dest_img = undef;
+	$source_img = undef;
 }
